53a1c603f29c86ad6dcc3dde1e2337b966e42b5b
Jonathan Nascimento do Prado <nerdgamer7495@gmail.com>
2026-09-02 18:47:00 -0300

    Adição questão 7 e cabeçalho de identificação do grupo + integrantes.

--- Atividade.sql
+++ Atividade.sql
@@ -1,22 +1,38 @@
--- create
+/*
+Tarefa de Banco de Dados - AC1
+Grupo G1 - TIN1.
+Integrantes + Questão que resolveu
+Ana Paula Alves Vieira (3 e 4)
+Gabriel Kiyoshi Hasshimoto (1 e 2)
+Jonathan Nascimento do Prado (7)
+Lucas Santana Souza (8 e 9)
+Mayar Ballan (10)
+Roni Santos Silva Júnior (5 e 6)
+ (
+*/
+
+-- 1. Criação das tabelas (A FAZER)
 CREATE TABLE EMPLOYEE (
   empId IDENTITY INT PRIMARY KEY,
   name TEXT NOT NULL,
   dept TEXT NOT NULL
 );
 
--- insert
-INSERT INTO EMPLOYEE VALUES ('Clark', 'Sales');
-INSERT INTO EMPLOYEE VALUES ('Dave', 'Accounting');
-INSERT INTO EMPLOYEE VALUES ('Ava', 'Sales');
 
--- fetch 
-SELECT * FROM EMPLOYEE WHERE dept = 'Sales';
-
- --  3. Alteração de tipo de dados _ _ _
+ --  3. Alteração de tipo de dados
 ALTER TABLE Classe
 ALTER COLUMN Caracteristicas VARCHAR(1500);
 
---  4. Exclusão de tipo de dado_ _ _ 
+--  4. Exclusão de campo
 ALTER TABLE raca
 DROP COLUMN perdido;
+
+-- 7. Atualização de dados
+UPDATE Personagem
+SET Poder += 10
+WHERE IDClasse IN
+(
+SELECT IDClasse
+FROM Classe
+WHERE Nome = 'Guerreiro'
+);
